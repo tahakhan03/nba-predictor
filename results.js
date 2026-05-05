34d54864126a665bfd6829728e85190958816005
Taha Khan <tahakhan0611@gmail.com>
2026-05-05 16:12:21 -0400

    first round results complete

--- results.js
+++ results.js
@@ -14,33 +14,39 @@
 
 const RESULTS = {
   east: {
+    // Round 0 = First Round
     0: {
-      0: { winner: null, games: null },  // 1 DET vs 8 ORL
-      1: { winner: null, games: null },  // 4 CLE vs 5 TOR
-      2: { winner: null, games: null },  // 3 NYK vs 6 ATL
-      3: { winner: null, games: null },  // 2 BOS vs 7 PHI
+      0: { winner: 'DET', games: 3 },  // 1 DET def. 8 ORL  4-3
+      1: { winner: 'CLE', games: 3 },  // 4 CLE def. 5 TOR  4-3
+      2: { winner: 'NYK', games: 2 },  // 3 NYK def. 6 ATL  4-2
+      3: { winner: 'PHI', games: 3 },  // 7 PHI def. 2 BOS  4-3
     },
+    // Round 1 = Conf. Semifinals
     1: {
-      0: { winner: null, games: null },  // Winner (DET/ORL) vs Winner (CLE/TOR)
-      1: { winner: null, games: null },  // Winner (NYK/ATL) vs Winner (BOS/PHI)
+      0: { winner: null, games: null },  // DET vs CLE
+      1: { winner: null, games: null },  // NYK vs PHI
     },
+    // Round 2 = Conf. Finals
     2: {
-      0: { winner: null, games: null },  // East Conference Finals
+      0: { winner: null, games: null },
     },
   },
   west: {
+    // Round 0 = First Round
     0: {
-      0: { winner: null, games: null },  // 1 OKC vs 8 PHX
-      1: { winner: null, games: null },  // 4 LAL vs 5 HOU
-      2: { winner: null, games: null },  // 3 DEN vs 6 MIN
-      3: { winner: null, games: null },  // 2 SAS vs 7 POR
+      0: { winner: 'OKC', games: 0 },  // 1 OKC def. 8 PHX  4-0
+      1: { winner: 'LAL', games: 2 },  // 4 LAL def. 5 HOU  4-2
+      2: { winner: 'MIN', games: 2 },  // 6 MIN def. 3 DEN  4-2
+      3: { winner: 'SAS', games: 1 },  // 2 SAS def. 7 POR  4-1
     },
+    // Round 1 = Conf. Semifinals
     1: {
-      0: { winner: null, games: null },  // Winner (OKC/PHX) vs Winner (LAL/HOU)
-      1: { winner: null, games: null },  // Winner (DEN/MIN) vs Winner (SAS/POR)
+      0: { winner: null, games: null },  // OKC vs LAL
+      1: { winner: null, games: null },  // MIN vs SAS
     },
+    // Round 2 = Conf. Finals
     2: {
-      0: { winner: null, games: null },  // West Conference Finals
+      0: { winner: null, games: null },
     },
   },
   finals: {
